e5c41d947a9942f4a1bba525fe0f83759c5ad116
linalpatle326-dev <linalpatle326@gmail.com>
2026-05-22 20:52:17 +0530

    Update gallery.js

--- task-1-image-gallery/gallery.js
+++ task-1-image-gallery/gallery.js
@@ -2,22 +2,62 @@ const images = document.querySelectorAll(".gallery img");
 
 const popup = document.getElementById("popup");
 const popupImg = document.getElementById("popupImg");
+
 const closeBtn = document.getElementById("closeBtn");
 
-images.forEach((img) => {
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+let currentIndex = 0;
+
+// Open Popup
+
+images.forEach((img, index) => {
 
     img.addEventListener("click", () => {
 
+        currentIndex = index;
+
         popup.style.display = "flex";
 
-        popupImg.src = img.src;
+        popupImg.src = images[currentIndex].src;
 
     });
 
 });
 
+// Close Popup
+
 closeBtn.addEventListener("click", () => {
 
     popup.style.display = "none";
+
+});
+
+// Next Image
+
+nextBtn.addEventListener("click", () => {
+
+    currentIndex++;
+
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+
+    popupImg.src = images[currentIndex].src;
+
+});
+
+// Previous Image
+
+prevBtn.addEventListener("click", () => {
+
+    currentIndex--;
+
+    if(currentIndex < 0){
+        currentIndex = images.length - 1;
+    }
+
+    popupImg.src = images[currentIndex].src;
 
 });
